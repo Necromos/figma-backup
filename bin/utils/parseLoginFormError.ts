1148199ad9476d79847af388218430bdfce30153
Necromos <necromos@necroworks.net>
2022-09-22 13:14:46 +0200

    style: next batch of bad disables

--- bin/utils/parseLoginFormError.ts
+++ bin/utils/parseLoginFormError.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Page } from "puppeteer";
 import findElementHandle from "./findElementHandle";
 
